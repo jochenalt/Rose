70008269d1ef7ec0a5f7a118889ed22e9a2b52e5
Jochen Alt <jochen.alt@gmx.net>
2018-03-25 06:30:14 -0400

    renamed

--- code/Server/src/audio/Playback.cpp
+++ code/Server/src/audio/Playback.cpp
@@ -56,6 +56,7 @@ void Playback::setup(int sampleRate) {
 void Playback::play(double volume /* 0..1 */ ,float outputBuffer[], int outputBufferSize) {
 	if (!playback)
 		return;
+
 	char playBuffer[outputBufferSize*2];
 	int outputBufferCount = 0;
 	for (int i = 0;i<outputBufferSize;i++) {
